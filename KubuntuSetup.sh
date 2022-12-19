37135c8a3afa39ba6d2b615191e209c938d366ba
Michael Tomanelli <115391279+mike-tmnly@users.noreply.github.com>
2022-12-18 20:58:01 -0800

    Removed Non-Latin Fonts
    Removed all Non-Latin fonts except for those provided by Noto and preceded package removal command with a command to remove any lingering UUID files that would prevent clean removal.

--- KubuntuSetup.sh
+++ KubuntuSetup.sh
@@ -98,4 +98,99 @@ sudo sed -i 's/quiet/quiet loglevel=0 mitigations=off/g' /etc/default/grub
 sudo update-grub2
 clear
 
+# Cleanup #
+echo "Removing Unwanted Fonts..."; sleep 1
+sudo find /usr/share/fonts -type f -iname .uuid -delete
+sudo apt purge -y \
+  fonts-beng \
+  fonts-beng-extra \
+  fonts-deva \
+  fonts-deva-extra \
+  fonts-gargi \
+  fonts-gubbi \
+  fonts-gujr \
+  fonts-gujr-extra \
+  fonts-guru \
+  fonts-guru-extra \
+  fonts-indic \
+  fonts-kacst \
+  fonts-kacst-one \
+  fonts-kalapi \
+  fonts-khmeros-core \
+  fonts-knda \
+  fonts-lao \
+  fonts-lklug-sinhala \
+  fonts-lohit-beng-assamese \
+  fonts-lohit-beng-bengali \
+  fonts-lohit-deva \
+  fonts-lohit-gujr \
+  fonts-lohit-guru \
+  fonts-lohit-knda \
+  fonts-lohit-mlym \
+  fonts-lohit-orya \
+  fonts-lohit-taml \
+  fonts-lohit-taml-classical \
+  fonts-lohit-telu \
+  fonts-mlym \
+  fonts-nakula \
+  fonts-navilu \
+  fonts-orya \
+  fonts-orya-extra \
+  fonts-pagul \
+  fonts-sahadeva \
+  fonts-samyak-deva \
+  fonts-samyak-gujr \
+  fonts-samyak-mlym \
+  fonts-samyak-taml \
+  fonts-sarai \
+  fonts-sil-abyssinica \
+  fonts-sil-padauk \
+  fonts-smc \
+  fonts-smc-anjalioldlipi \
+  fonts-smc-chilanka \
+  fonts-smc-dyuthi \
+  fonts-smc-gayathri \
+  fonts-smc-karumbi \
+  fonts-smc-keraleeyam \
+  fonts-smc-manjari \
+  fonts-smc-meera \
+  fonts-smc-rachana \
+  fonts-smc-raghumalayalamsans \
+  fonts-smc-suruma \
+  fonts-smc-uroob \
+  fonts-taml \
+  fonts-telu \
+  fonts-telu-extra \
+  fonts-teluguvijayam \
+  fonts-thai-tlwg \
+  fonts-tibetan-machine \
+  fonts-tlwg-garuda \
+  fonts-tlwg-garuda-ttf \
+  fonts-tlwg-kinnari \
+  fonts-tlwg-kinnari-ttf \
+  fonts-tlwg-laksaman \
+  fonts-tlwg-laksaman-ttf \
+  fonts-tlwg-loma \
+  fonts-tlwg-loma-ttf \
+  fonts-tlwg-mono \
+  fonts-tlwg-mono-ttf \
+  fonts-tlwg-norasi \
+  fonts-tlwg-norasi-ttf \
+  fonts-tlwg-purisa \
+  fonts-tlwg-purisa-ttf \
+  fonts-tlwg-sawasdee \
+  fonts-tlwg-sawasdee-ttf \
+  fonts-tlwg-typewriter \
+  fonts-tlwg-typewriter-ttf \
+  fonts-tlwg-typist \
+  fonts-tlwg-typist-ttf \
+  fonts-tlwg-typo \
+  fonts-tlwg-typo-ttf \
+  fonts-tlwg-umpush \
+  fonts-tlwg-umpush-ttf \
+  fonts-tlwg-waree \
+  fonts-tlwg-waree-ttf \
+  fonts-yrsa-rasa
+clear
+
 # END-OF-SCRIPT #
